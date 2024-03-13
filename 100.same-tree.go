72d613443cd1e76c7da038bb1609df205a58c841
naoya7076 <naoya7076@gmail.com>
2024-03-13 16:03:36 +0900

    2分探索木の比較をする問題の回答を追加

---
+++ 100.same-tree.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=100 lang=golang
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	leftTree := isSameTree(p.Left, q.Left)
+	rightTree := isSameTree(p.Right, q.Right)
+	return leftTree && rightTree
+}
+
+// @lc code=end
