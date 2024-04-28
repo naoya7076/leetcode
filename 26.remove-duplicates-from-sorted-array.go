b2d349225a8e9dd9e4af23bf3b0eaab9af9c958d
naoya7076 <naoya7076@gmail.com>
2024-04-28 21:31:13 +0900

    26の回答

---
+++ 26.remove-duplicates-from-sorted-array.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+package main
+
+// numsの
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	lastUniqueIndex := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[lastUniqueIndex] {
+			lastUniqueIndex++
+			nums[lastUniqueIndex] = nums[i]
+		}
+	}
+	return lastUniqueIndex + 1
+}
+
+// @lc code=end
